62f54a84a40b129c2320380aa7230cbab734bab6
unknown <peted70@hotmail.com>
2017-12-31 18:13:00 +0000

    added WinRT component interface

--- WinRTGLTFParser/GLTF_Parser.cpp
+++ WinRTGLTFParser/GLTF_Parser.cpp
@@ -1,9 +1,36 @@
 ﻿#include "stdafx.h"
 #include "GLTF_Parser.h"
+#include <fstream>
 
-using namespace WinRTGLTFParser;
 using namespace Platform;
+using namespace WinRTGLTFParser;
 
 GLTF_Parser::GLTF_Parser()
 {
+}
+
+String^ ToStringHat(char* ch)
+{
+	std::string s_str = std::string(ch);
+	std::wstring wid_str = std::wstring(s_str.begin(), s_str.end());
+	const wchar_t* w_char = wid_str.c_str();
+	String^ p_string = ref new String(w_char);
+	return p_string;
+}
+
+void GLTF_Parser::ParseFile(String^ Filename)
+{
+	std::ifstream infile(Filename->Data(), std::ios::binary);
+
+	if (infile.fail())
+	{
+		throw Platform::Exception::CreateException(E_FAIL, ToStringHat(strerror(errno)));
+	}
+
+	::ParseFile(infile, [this](const BufferData& data)
+	{
+		OnBufferEvent(data);
+	});
+
+	infile.close();
 }
--- WinRTGLTFParser/GLTF_Parser.h
+++ WinRTGLTFParser/GLTF_Parser.h
@@ -1,10 +1,32 @@
 ﻿#pragma once
 
+#include <stdafx.h>
+#include <istream>
+#include "../SharedGLTFParser/gltfparser.h"
+
 namespace WinRTGLTFParser
 {
-    public ref class GLTF_Parser sealed
+	using namespace Windows::Storage::Streams;
+	using namespace Platform;
+	using namespace std;
+
+	public delegate void BufferEventHandler(const BufferData&);
+
+	public ref class GLTF_Parser sealed
     {
     public:
 		GLTF_Parser();
+
+		event BufferEventHandler^ OnBufferEvent;
+
+		/// <summary>
+		/// Will parse a .GLB file given the input filename
+		/// Note. this would ideally be a stream interface passed in but since I would need
+		/// to write the code to interface a WinRT stream and native C++ stream I am avoinding it
+		/// for now. The native implementation uses std::istream. 
+		/// </summary>
+		/// <param name="Filename"></param>
+		/// <returns></returns>
+		void ParseFile(String^ Filename);
     };
 }
